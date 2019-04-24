a8bf6d19864cff1d2acd45d631ece2c79f581bf7
Müller Heiko <hmuller@ulapland.fi>
2019-04-24 13:04:04 +0300

    Skript to realise PWM with digitalWrite using microsecond pulses.

--- ManualPWM/ManualPWM.ino
+++ ManualPWM/ManualPWM.ino
@@ -1,22 +1,49 @@
+/*
+  ManualPWM
+
+  This example shows how to create PWM by using the micros-function and avoiding analogWrite
+
+  Using micros, there is no need using analogWrite, which overrides all pinmode-settings
+  and thus disables any of the built-in pull-up resistors. Another advantage is that the
+  code is free of delay-commands.
+
+  Creating a manual PWM will still pulse bursts of 5v into your displays, which may
+  lead to deterioration of the electrolyte and aging of the display.
+
+  A variable using unsigned long to store the micros will overflow after approx. 70 minutes after 
+  the Arduino started running.
+  
+  by Heiko Müller
+  https://github.com/DecoChrom/Arduino/ManualPWM
+  
+*/
+
+//time-keeping variables
 unsigned long now = 0;
 unsigned long lastChange = 0;
+// this will give you ~1.5v readout on the voltmeter
 int deltaHigh = 360;
 int deltaLow = 140;
+//needed to alternate states
 bool flag = false;
 
 void setup() {
-  // put your setup code here, to run once:
+  // setting pins 5 and 6 to output
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
 }
 
 void loop() {
+  //reading current runtime in micro-seconds
   now = micros();
+  //checking for overflow, which will happen approx. 70 minutes after program started running
   if (now < lastChange)
   {
     lastChange = now;
   }
+  //setting pin 5 to ground
   digitalWrite(5, LOW);
+  //alternating between HIGH and LOW on pin 6 depending on values deltaHigh and deltaLow
   if ((now-lastChange > deltaHigh) && !flag)
   {
     lastChange = now;
